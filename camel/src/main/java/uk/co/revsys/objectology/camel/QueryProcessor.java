314bd9fa88225ac886a1adbcc5484c0c36c96e96
Adam Knight <adam@revolutionarysystems.co.uk>
2014-12-08 09:44:54 +0000

    Added view param to QueryProcessor

--- camel/src/main/java/uk/co/revsys/objectology/camel/QueryProcessor.java
+++ camel/src/main/java/uk/co/revsys/objectology/camel/QueryProcessor.java
@@ -1,11 +1,16 @@
 package uk.co.revsys.objectology.camel;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.camel.Exchange;
 import uk.co.revsys.esb.component.HttpProxyProcessor;
 
 public class QueryProcessor extends HttpProxyProcessor {
 
     private String type;
+    private String view;
     
     public QueryProcessor(String baseUrl) {
         super(baseUrl);
@@ -19,6 +24,14 @@ public class QueryProcessor extends HttpProxyProcessor {
         this.type = type;
     }
 
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
     @Override
     public String getHttpMethod() {
         return POST;
@@ -27,6 +40,19 @@ public class QueryProcessor extends HttpProxyProcessor {
     @Override
     public String getUrlPath(Exchange exchange) {
         return "/" + getType() + "/query";
+    }
+
+    @Override
+    public String getQueryString() {
+        if(view!=null){
+            try {
+                return "view=" + URLEncoder.encode(view, "UTF-8");
+            } catch (UnsupportedEncodingException ex) {
+                return "view=" + view;
+            }
+        }else{
+            return null;
+        }
     }
 
     @Override
