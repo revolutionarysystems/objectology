efbaf7c94c5ea76dad9b09996e086525be57b5c4
Adam Knight <adam@revolutionarysystems.co.uk>
2015-06-16 14:08:00 +0100

    Fixed property deserialisation with null constraint

--- core/src/main/java/uk/co/revsys/objectology/model/template/PropertyTemplate.java
+++ core/src/main/java/uk/co/revsys/objectology/model/template/PropertyTemplate.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import uk.co.revsys.objectology.exception.ValidationException;
 import uk.co.revsys.objectology.model.instance.Property;
 
-public class PropertyTemplate extends AtomicAttributeTemplate<Property>{
+public class PropertyTemplate extends AtomicAttributeTemplate<Property> {
 
     private String constraint;
     @JsonIgnore
@@ -17,16 +17,18 @@ public class PropertyTemplate extends AtomicAttributeTemplate<Property>{
 
     public void setConstraint(String constraint) {
         this.constraint = constraint;
-        this.pattern = Pattern.compile(constraint);
+        if (constraint != null) {
+            this.pattern = Pattern.compile(constraint);
+        }
     }
 
     @Override
     public void doValidate(Property attribute) throws ValidationException {
-        if(attribute!=null && attribute.getValue()!=null && pattern!=null && !pattern.matcher(attribute.getValue()).matches()){
+        if (attribute != null && attribute.getValue() != null && pattern != null && !pattern.matcher(attribute.getValue()).matches()) {
             throw new ValidationException("Expected " + constraint);
         }
     }
-    
+
     @Override
     public Class<? extends Property> getAttributeType() {
         return (Class<? extends Property>) Property.class;
